a0465a612e7b2edf68ab4d6ce53c91e42ef0fd1b
Charlie Mchaileh <s3947173@student.rmit.edu.au>
2024-10-02 19:24:59 +1000

    Gallery converted to php

--- a2/gallery.php
+++ a2/gallery.php
@@ -5,45 +5,39 @@ include('Includes/header.inc');
 include('Includes/nav.inc');
 ?>
 <body>
-    <main>
-        <div class="gallery-text">
+    <main> 
+    <div class="gallery-text">
           <h1>Pets Victoria has a lot to offer!</h1>
           <p>For almost two decades, Pets Victoria has helped in creating true social change by bringing pet adoption into the mainstream. Our work has helped make a difference to the Victorian rescue community and thousands of pets in need of rescue and rehabilitation. But, until every pet is safe, respected, and loved, we all still have big, hairy work to do.</p>
-        </div>
-        <div class="pets-grid">
-            <div class="pet-card">
-                <img src="images\images\cat1.jpeg" alt="Milo">
-                <div class="overlay">
-                    <a href="#">Discover more!</a>
-
-                </div>
-                <h2>Milo</h2>
-            </div>
-            <div class="pet-card">
-                <img src="images\images\dog1.jpeg" alt="Baxter">
-                <h2>Baxter</h2>
-            </div>
-            <div class="pet-card">
-                <img src="images\images\cat2.jpeg" alt="Luna">
-                <h2>Luna</h2>
-            </div>
-            <div class="pet-card">
-                <img src="images\images\dog2.jpeg" alt="Willow">
-                <h2>Willow</h2>
-            </div>
-            <div class="pet-card">
-                <img src="images\images\cat4.jpeg" alt="Oliver">
-                <h2>Oliver</h2>
-            </div>
-            <div class="pet-card">
-                <img src="images\images\dog3.jpeg" alt="Bella">
-                <h2>Bella</h2>
-            </div>
-        </div>
+    </div>`
+    <div class="pets-grid">
+    <?php
+    include('Includes/db_connect.inc');
+    $sql = "select * from pets";
+    $result = $conn->query($sql);  
+            
+            if ($result->num_rows > 0) {
+                // Output data for each pet
+                while($row = $result->fetch_assoc()) {
+                    echo '<div class="pet-card">';
+                    echo '<img src="' . $row["image"] . '" alt="' . $row["petname"] . '">';
+                    echo '<div class="overlay">';
+                    echo '<a href="details.php?petid=' . $row["petid"] . '">Discover more!</a>';
+                    echo '</div>';
+                    echo '<h2>' . $row["petname"] . '</h2>';
+                    echo '</div>';
+                }
+            } else {
+                echo "No pets found.";
+            }
+            
+        
+    ?>
+    </div>
     </main>
-
 </body>
 
 <?php
+$conn->close();
 include('Includes/footer.inc')
 ?>
