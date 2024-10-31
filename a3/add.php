1d5254ab2506e7d494406911952024e3dcdc1537
Charlie Mchaileh <s3947173@student.rmit.edu.au>
2024-10-31 22:48:53 +1100

    added responsive bootstrap

--- a3/add.php
+++ a3/add.php
@@ -17,32 +17,40 @@ if (!isset($_SESSION['username'])) {
     <form action="process_form.php" method="post" enctype="multipart/form-data">
             <h2>Add a pet</h2>
             <p>You can add a new pet here</p>
-
-            <label for="petname">Pet Name:</label>
-            <input type="text" id="petname" name="petname" placeholder="Provide a name for the pet" required>
-
-            <label for="type">Type:</label>
-            <input type="text" id="type" name="type" placeholder="Provide type" required>
-
-            <label for="description">Description:</label>
-            <textarea id="description" name="description" placeholder="Describe the pet briefly" required></textarea>
-
-            <label for="image">Select an Image:</label>
-            <input type="file" id="image" name="image" accept="image/*" required>
-            <small>Max image size: 500px</small>
-
-            <label for="caption">Image Caption:</label>
-            <input type="text" id="caption" name="caption" placeholder="Describe the image in one word" required>
-
-            <label for="age">Age (months):</label>
-            <input type="number" id="age" name="age" placeholder="Age of a pet in months" required>
-
-            <label for="location">Location:</label>
-            <input type="text" id="location" name="location" placeholder="Location of the pet" required>
-
-            <div class="form-buttons">
-                <button type="submit" class="submit-btn">Submit</button>
-                <button type="reset" class="clear-btn">Clear</button>
+            <div class="mb-3 mt-3">
+              <label for="petname">Pet Name:</label>
+              <input type="text" id="petname" name="petname" placeholder="Provide a name for the pet" class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+              <label for="type">Type:</label>
+              <input type="text" id="type" name="type" placeholder="Provide type" class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+              <label for="description">Description:</label>
+              <textarea id="description" name="description" placeholder="Describe the pet briefly" class="form-control w-50" required></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="image">Select an Image:</label>
+              <input type="file" id="image" name="image" accept="image/*"  class="form-control w-50" required>
+              <small>Max image size: 500px</small>
+            </div>
+            <div class="mb-3">
+              <label for="caption">Image Caption:</label>
+              <input type="text" id="caption" name="caption" placeholder="Describe the image in one word"  class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+              <label for="age">Age (months):</label>
+              <input type="number" id="age" name="age" placeholder="Age of a pet in months"  required min="1900" max="2021" size="4" class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+              <label for="location">Location:</label>
+              <input type="text" id="location" name="location" placeholder="Location of the pet"  class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+                <div class="form-buttons">
+                    <button type="submit" class="submit-btn">Submit</button>
+                    <button type="reset" class="clear-btn">Clear</button>
+                </div>
             </div>
     </form>
     </div>
